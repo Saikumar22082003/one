590e3e3536ff2a1b2c815d133ec5e52669e195af
Saikumar22082003 <sai22082003@gmail.com>
2024-11-25 10:45:04 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,6 +1,19 @@
-class 1{
-    public static void main (string args)[])
-    {
-        System.out.print("this is sai kumar");
+public class Prime {
+    public static void main(String[] args) {
+        int number = 29; // Example number
+        boolean isPrime = true;
+
+        for (int i = 2; i <= number / 2; ++i) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
     }
 }
